0144b15d5def5b82e5bc03a7ed0ac61a11474fcc
Felipe635 <102757641+Felipe635@users.noreply.github.com>
2022-05-04 16:45:13 -0300

    Add files via upload

---
+++ exercicios.js
@@ -0,0 +1,42 @@
+//1-a
+console.log('Exercicío 1')
+console.log(10+20*30);
+
+//1-b
+console.log(4**2/30);
+
+//1-c
+console.log((9**4+2)*6-1);
+console.log('****************')
+//2
+console.log('Exercicío 2')
+console.log(10%3*10**1+1-10*4/2);
+console.log('****************')
+//3
+console.log('Exercicío 3')
+console.log('Felipe');
+console.log('****************')
+//4
+const x = 3;
+const y = 5;
+
+console.log('Exercicío 4')
+console.log(2*x*3*y);
+console.log('****************')
+//5
+const a = 3;
+const b = 5;
+const c = 2;
+
+console.log('Exercicío 5')
+console.log(a+b+c);
+console.log('****************')
+//6
+const s = 1800;
+
+console.log('Exercicío 6')
+console.log(s*2);
+console.log('****************')
+
+
+
